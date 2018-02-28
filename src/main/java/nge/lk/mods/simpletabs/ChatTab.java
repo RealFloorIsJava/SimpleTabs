64c7d1fe846d351a7128ad86435582ecd5a05c82
L. K <31963040+LordKorea@users.noreply.github.com>
2018-02-28 23:57:44 +0100

    Initial commit

---
+++ src/main/java/nge/lk/mods/simpletabs/ChatTab.java
@@ -0,0 +1,100 @@
+package nge.lk.mods.simpletabs;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiNewChat;
+import net.minecraft.util.text.ITextComponent;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Represents a chat tab.
+ */
+public class ChatTab extends GuiNewChat {
+
+    /**
+     * The filter which selects the messages to accept.
+     */
+    private Matcher filter;
+
+    /**
+     * Whether this tab has unread messages.
+     */
+    @Getter private boolean unread;
+
+    /**
+     * The pattern string, for saving.
+     */
+    @Getter private String pattern;
+
+    /**
+     * Whether the pattern is literal, for saving.
+     */
+    @Getter private boolean literal;
+
+    /**
+     * The prefix for sent messages in this tab.
+     */
+    @Getter @Setter private String prefix;
+
+    /**
+     * Constructor.
+     *
+     * @param mc The minecraft reference.
+     */
+    public ChatTab(final Minecraft mc, final String pattern, final boolean literal, final String prefix) {
+        super(mc);
+        this.pattern = pattern;
+        this.literal = literal;
+        this.prefix = prefix;
+        filter = Pattern.compile(pattern, literal ? Pattern.LITERAL : 0).matcher("");
+    }
+
+    /**
+     * Updates the filter pattern.
+     *
+     * @param pattern The new pattern.
+     * @param literal Whether the pattern will be escaped.
+     */
+    public void updatePattern(final String pattern, final boolean literal) {
+        this.pattern = pattern;
+        this.literal = literal;
+        filter = Pattern.compile(pattern, literal ? Pattern.LITERAL : 0).matcher("");
+    }
+
+    @Override
+    public void printChatMessageWithOptionalDeletion(final ITextComponent chatComponent, final int chatLineId) {
+        super.printChatMessageWithOptionalDeletion(chatComponent, chatLineId);
+        unread = true;
+    }
+
+    /**
+     * Whether this tab accepts the given message for display.
+     *
+     * @param message The message.
+     *
+     * @return Whether it is accepted.
+     */
+    public boolean acceptsMessage(final CharSequence message) {
+        filter.reset(message);
+        return filter.find();
+    }
+
+    /**
+     * Marks this tab as read.
+     */
+    public void markRead() {
+        unread = false;
+    }
+
+    /**
+     * Returns the export part of this tab.
+     *
+     * @return The export string.
+     */
+    public String getExport() {
+        return pattern + "§" + Boolean.toString(literal) + "§" + prefix;
+    }
+}
